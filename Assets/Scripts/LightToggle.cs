ec10be38e8a67fdd0a8b412e7d5a51e69b0a05df
Raphaël Côté <cotlarrc@msn.com>
2018-09-24 22:54:53 -0400

    Added time indicator lol

--- Assets/Scripts/LightToggle.cs
+++ Assets/Scripts/LightToggle.cs
@@ -6,28 +6,44 @@ public class LightToggle : MonoBehaviour {
 	
 	[SerializeField]
 	public DayController daylight;
+	[SerializeField]
+	public GameObject timer;
+	[SerializeField]
+	public float tempsPourAppuyer = 2;
 
 	private int nombreDeJoueursSurLeBouton = 0;
-	private float tempsRestant = 2;
+	private GameObject activeTimer;
+
+	void DeleteTimer() {
+		Destroy(activeTimer);
+	}
+
+	void NewTimer() {
+		var t = Instantiate(timer, transform);
+		t.transform.Translate(0, 1, 0);
+		t.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+		
+		t.GetComponent<Timer>().temps = tempsPourAppuyer;
+		activeTimer = t;
+	}
 
 	void OnTriggerExit2D() {
 		nombreDeJoueursSurLeBouton--;
 		if (nombreDeJoueursSurLeBouton == 0) {
-			tempsRestant = 2;
+			DeleteTimer();
 		}
-		Debug.Log(nombreDeJoueursSurLeBouton);
 	}
 
 	void OnTriggerEnter2D() {
+		if (nombreDeJoueursSurLeBouton == 0) {
+			NewTimer();
+		}
 		nombreDeJoueursSurLeBouton++;
-		Debug.Log(nombreDeJoueursSurLeBouton);
 	}
 
 	void Update() {
 		if (nombreDeJoueursSurLeBouton > 0) {
-			tempsRestant -= Time.deltaTime;
-			Debug.Log(tempsRestant);
-			if (tempsRestant <= 0) {
+			if (activeTimer.GetComponent<Timer>().TimeUp()) {
 				daylight.Toggle();
 				Destroy(gameObject);
 			}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+	public float temps = 2;
+
+    private Transform indicateur;
+	private float vitesse;
+
+    // Use this for initialization
+    void Start () {
+		indicateur = transform.GetChild(0);
+		vitesse = indicateur.gameObject.GetComponent<SpriteRenderer>().bounds.size.x / temps;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (temps > 0) {
+			temps -= Time.deltaTime;
+			indicateur.Translate(Time.deltaTime * -vitesse, 0, 0);
+		}
+	}
+
+	public bool TimeUp()
+	{
+		return temps <= 0;
+	}
+}
